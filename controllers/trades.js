3cd8adef079e62789458c1cbe85b228acaae0e6c
User <user@rba-hackerrank.com>
2019-03-15 00:45:07 +0000

    [HackerRank] Auto commit on Run Tests

--- controllers/trades.js
+++ controllers/trades.js
@@ -2,7 +2,8 @@ module.exports = function(app) {
     var controller = {}
     
     controller.removeTransactions = function (req, res){
-        transactions.remove().exec()
+        var _id = req.params.id;
+        transactions.remove({"_id": _id }).exec()
         .then(
             function() {
                 res.status(201).end();
